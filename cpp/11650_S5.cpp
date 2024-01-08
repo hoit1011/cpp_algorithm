40f900e2d6e263e92df3fa8705e04bd85e5e2cfd
hoit1011 <parksm071011@gmail.com>
2024-01-08 11:19:38 +0900

    11650_S5

---
+++ cpp/11650_S5.cpp
@@ -0,0 +1,29 @@
+//
+// Created by 박성민 on 1/8/24.
+//
+//
+// Created by 박성민 on 1/8/24.
+// 한줄평 : 모르겠어서 검색을 해서 vector에 pair begin등 queue와 다른점을알게됐는데 저 코드가 백준에 제출하면 되긴하는데 에디터에선 오류가 난다 (clion 환경)
+
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int main() {
+//    vector<pair<int, int>> v;
+//
+//    int N;
+//    cin >> N;
+//    int x, y;
+//    for (int i = 0; i < N; i++) {
+//        cin >> x >> y;
+//        v.push_back({ x, y });
+//    }
+//    sort(v.begin(), v.end());
+//    for (int i = 0; i < N; i++) {
+//        cout << v[i].first << ' ' << v[i].second << '\n';
+//    }
+//    return 0;
+//}
