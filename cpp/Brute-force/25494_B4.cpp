865c574d3dc2a7096f0f2f61e13b2c6672acc8fb
hoit1011 <parksm071011@gmail.com>
2024-02-26 11:56:50 +0900

    25494_B4 + Brute-force 디렉토리 정리

---
+++ cpp/Brute-force/25494_B4.cpp
@@ -0,0 +1,28 @@
+//
+// Created by 박성민 on 2/26/24.
+//
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//    int N;
+//    cin >> N;
+//    for(int i = 1; i <= N; i++){
+//        int a;
+//        int b;
+//        int c;
+//        int min = 100;
+//        cin >> a >> b >> c;
+//        if(a < min)
+//            min = a;
+//        if(b < min)
+//            min = b;
+//        if(c < min)
+//            min = c;
+//
+//        cout << min << '\n';
+//    }
+//    return 0;
+//}
