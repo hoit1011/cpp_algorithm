beedde9bea2c0746cf1b538143ff27e481368e36
hoit1011 <parksm071011@gmail.com>
2024-02-27 18:06:45 +0900

    10987_B4

---
+++ cpp/10987_B4.cpp
@@ -0,0 +1,20 @@
+//
+// Created by 박성민 on 2/27/24.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string a;
+    cin >> a;
+    int cnt = 0;
+    for(int i = 0; i < a.length(); i++){
+        if(a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u')
+            cnt++;
+    }
+    cout << cnt;
+
+    return 0;
+}
---
+++ cpp/Brute-force/1837_B3_X.cpp
@@ -0,0 +1,30 @@
+//
+// Created by 박성민 on 2/26/24.
+//
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//    int N;
+//    int pri;
+//    int good = 0;
+//    cin >> N >> pri;
+//    int min = 999999999;
+//    for(int i = 1; i <= N; i++){
+//        int a;
+//        int b;
+//        if (N % i == 0){
+//            a = i;
+//            b = N / i;
+//            if(a >= pri && b >= pri){
+//                good = 1;
+//                break;
+//            }
+//            else{
+//
+//            }
+//        }
+//    }
+//}
