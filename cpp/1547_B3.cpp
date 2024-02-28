bd777229ce969854059bcf291579434704dcb603
hoit1011 <parksm071011@gmail.com>
2024-02-28 22:31:43 +0900

    1547_B3

---
+++ cpp/1547_B3.cpp
@@ -0,0 +1,32 @@
+//
+// Created by 박성민 on 2/28/24.
+// 한줄평 : swap이라는 것을 알게 되었다
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int arr[4] = {0, 1, 0, 0};
+//
+//int main() {
+//    int N;
+//    cin >> N;
+//
+//    for (int i = 0; i < N; i++) {
+//        int a, b;
+//        cin >> a >> b;
+//        swap(arr[a], arr[b]);
+//    }
+//
+//    int num = 0;
+//    for (int i = 1; i <= 3; i++) {
+//        if (arr[i] == 1) {
+//            num = i;
+//            break;
+//        }
+//    }
+//
+//    cout << num;
+//
+//    return 0;
+//}
