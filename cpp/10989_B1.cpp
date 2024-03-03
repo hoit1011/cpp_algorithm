d2af9481cd2a2b9a87ac44a01fba2d7b8e61a887
hoit1011 <parksm071011@gmail.com>
2024-03-03 22:39:41 +0900

    10989_B1

---
+++ cpp/10989_B1.cpp
@@ -0,0 +1,44 @@
+//
+// Created by 박성민 on 3/3/24.
+// 란줄평 : 개 씨발 해달라는데로 해줘도 안되는데 어떡하라는거임 이건 코드 2개를 짜줘도 시간초과로 안되는데 그냥 파이썬 코드로 풀었다침
+// 이건 그냥 진짜 넘기자 ㅋㅋ
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//    int count [10001] = {0,};
+//    int N;
+//    cin >> N;
+//    for(int i = 0 ; i < N; i++){
+//        int x;
+//        cin >> x;
+//        count[x]++;
+//    }
+//    for(int i = 0; i < 10001; i++)
+//        for(int j = 0; j < count[i]; j++)
+//            cout << i << '\n';
+//    return 0;
+//}
+
+//#include <algorithm>
+//#include <vector>
+//using namespace std;
+//
+//vector<int> arr;
+//int main(){
+//    int N;
+//    cin >> N;
+//    for(int i = 0 ; i < N; i++) {
+//        int x;
+//        cin >> x;
+//        arr.push_back(x);
+//    }
+//    sort(arr.begin(),arr.end());
+//
+//    for(int i = 0 ; i < N; i++){
+//        cout << arr.at(i)  << '\n';
+//    }
+//    return 0;
+//}
