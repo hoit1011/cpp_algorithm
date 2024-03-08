720d7d4b46be5fe64e47fb573fb4dd721b6a538f
hoit1011 <parksm071011@gmail.com>
2024-03-08 14:08:03 +0900

    9325_B3

---
+++ cpp/9325_B3.cpp
@@ -0,0 +1,26 @@
+////
+//// Created by 박성민 on 3/8/24.
+////
+//
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//    int N;
+//    cin >> N;
+//    for(int i = 0 ; i < N; i++){
+//        int price = 0;
+//        cin >> price;
+//        int n;
+//        cin >> n;
+//        for(int j = 0 ; j < n; j++){
+//            int cnt;
+//            int pri;
+//            cin >> cnt >> pri;
+//            price += cnt * pri;
+//        }
+//        cout << price << '\n';
+//    }
+//    return 0;
+//}
