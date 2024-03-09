a95ee65cd1d667fe66fa5933e3c97be14a5a1357
hoit1011 <parksm071011@gmail.com>
2024-03-09 11:37:37 +0900

    1357_B1

---
+++ cpp/1357_B1.cpp
@@ -0,0 +1,25 @@
+//
+// Created by 박성민 on 3/9/24.
+// 한줄평 : reverse를 알게됐고 string은 .begin과 .end를 쓸수있다는것을 알게되었고 stoi라는 int형으로 반환하는것도 알게되었다
+
+//#include <iostream>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int Rev(string x){
+//    reverse(x.begin(),x.end());
+//    int a = stoi(x);
+//    return a;
+//}
+//
+//int main(){
+//    string x,y;
+//    cin >> x >> y;
+//    int a = Rev(x);
+//    int b = Rev(y);
+//    int sum = a + b;
+//    int res = Rev(to_string(sum));
+//    cout << res;
+//    return 0;
+//}
