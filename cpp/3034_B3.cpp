4961e8c785da8083733cfbbbc622f86bb464b9a2
hoit1011 <parksm071011@gmail.com>
2024-03-10 23:04:04 +0900

    3034_B3

---
+++ cpp/3034_B3.cpp
@@ -0,0 +1,24 @@
+//
+// Created by 박성민 on 3/10/24.
+// 한줄평 : 대각선 구하는 문제였는데 제곱풀기 귀찮아서 걍 제곱해버림
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//    int n,x,y;
+//    cin >> n >> x >> y;
+//    int num = 0;
+//    num += x * x;
+//    num += y * y;
+//    for(int i = 0 ; i < n; i++){
+//        int N;
+//        cin >> N;
+//        if((N * N) <= num)
+//            cout << "DA" << '\n';
+//        else
+//            cout << "NE" << '\n';
+//    }
+//    return 0;
+//}
