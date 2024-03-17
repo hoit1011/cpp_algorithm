fdd5ff20eca35219a7fa1a217efec9fdc5e4cc12
hoit1011 <parksm071011@gmail.com>
2024-03-17 20:19:05 +0900

    1065_S4

---
+++ cpp/Brute-force/1065_S4.cpp
@@ -0,0 +1,27 @@
+//
+// Created by 박성민 on 3/17/24.
+// 한줄평 : 브루트포스알고리즘이 굉장히 재미있는것 같고 이제 슬슬 감을 잡은것같다 그리고 백,십,일의자리를 구하는 방법은 외워놔야겠다.
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//    int n;
+//    cin >> n;
+//    int cnt = 99;
+//    if(n < 100){
+//        cout << n;
+//    }else{
+//        for(int i = 100; i <= n; i++){
+//            int O,T,H;
+//            H = i / 100;
+//            T = (i / 10) % 10;
+//            O = i % 10;
+//            if(H + O == T * 2)
+//                cnt++;
+//        }
+//        cout << cnt;
+//    }
+//    return 0;
+//}
