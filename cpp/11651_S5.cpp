c8104aa36929393f14a0aacfda2aa22169da2a6a
hoit1011 <parksm071011@gmail.com>
2024-03-20 20:10:23 +0900

    11651_S5

---
+++ cpp/11651_S5.cpp
@@ -0,0 +1,22 @@
+// 한줄평 : vector안에 pair를 쓸수있다는 사실을 알았따 !
+
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//using namespace std;
+//int main(){
+//    int n;
+//    vector<pair<int,int> > v;
+//    cin >> n;
+//    for(int i = 0; i < n; i++){
+//        int a,b;
+//        cin >> a >> b;
+//        v.push_back(pair<int,int>(b,a));
+//    }
+//    sort(v.begin(),v.end());
+//    for(int i = 0; i < n; i++){
+//        cout << v[i].second << " " << v[i].first << '\n';
+//    }
+//    return 0;
+//
+//}
