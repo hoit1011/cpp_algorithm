46a02b54edc067cddf8c7f5427a0919b72013b64
hoit1011 <parksm071011@gmail.com>
2024-03-25 08:09:23 +0900

    1676_S5

--- cpp/1676_S5.cpp
+++ cpp/1676_S5.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
-
-using namespace std;
-
-int main(){
-    int ans = 0;
-
-    int N;
-    cin >> N;
-
-    for (int i=5; i<=N; i*=5)
-        ans += N/i;
-    cout << ans << '\n';
-
-    return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//    int ans = 0;
+//
+//    int N;
+//    cin >> N;
+//
+//    for (int i=5; i<=N; i*=5)
+//        ans += N/i;
+//    cout << ans << '\n';
+//
+//    return 0;
+//}
