33bb410518f7e89a11b53bafa9cde8f346d7696b
hoit1011 <parksm071011@gmail.com>
2024-03-24 22:52:04 +0900

    1676_S5

--- cpp/backtracking/15649_S3.cpp
+++ cpp/backtracking/15649_S3.cpp
@@ -1,6 +1,5 @@
 //한줄평 : 풀었다고는 했지만 사실 만족스럽지는 않다 다시 풀라고하면 확실하게 대답을 할수도 없을것같고 아직 정확하기 이해하지도 못한것같기 때문이다 앞으로도 N과M문제를 다 풀면서 공부를 해야겠다
 
-
 //#include <iostream>
 //
 //#define fast cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
