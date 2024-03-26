022badbd110eef42ebc75db603170a498e8c196f
hoit1011 <parksm071011@gmail.com>
2024-03-26 17:12:51 +0900

    15652_S3

---
+++ cpp/backtracking/15652_S3.cpp
@@ -0,0 +1,27 @@
+//한줄평 : 감을 잡다가도 모르겠다 이거 진짜 말이안된다 왜이러냐이거
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int N,M;
+//int arr[10] = {0,};
+//int dfs(int j , int k){
+//    if(k == M){
+//        for(int i = 0 ;  i < M ; i++){
+//            cout << arr[i] << ' ';
+//        }
+//        cout << '\n';
+//        return 0;
+//    }
+//    for(int i = j;i <= N; i++){
+//        arr[k] = i;
+//        dfs(i,k+1);
+//    }
+//    return 0;
+//}
+//int main(){
+//    cin >> N >> M;
+//    dfs(1,0);
+//    return 0;
+//}
