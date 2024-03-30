a4c10e69566c3a6e222c8499fc9da42e7b47ae61
hoit1011 <parksm071011@gmail.com>
2024-03-30 21:24:16 +0900

    10814_S5

---
+++ cpp/10814_S5.cpp
@@ -0,0 +1,31 @@
+//
+// Created by 박성민 on 3/30/24.
+//
+
+// 한줄평 : stable_sort라는 안전한 ? 정렬이라는것을 알게되었고 sort를 쓰는법을 한층 더 자세히 배운것 같다.
+
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//pair<int , string> p;
+//vector<pair<int, string> > v;
+//
+//int compare(pair<int,string> a, pair<int, string> b){
+//    return a.first < b.first;
+//}
+//
+//int main(){
+//    int n;
+//    cin >> n;
+//    for(int i = 0 ; i < n; i++){
+//        cin >> p.first >> p.second;
+//        v.push_back(p);
+//    }
+//    stable_sort(v.begin(), v.end(), compare);
+//    for(int i = 0; i < n; i++){
+//        cout << v[i].first << ' ' << v[i].second << '\n';
+//    }
+//}
