d741a6fb908f046d85867a95f8bb404311cba1f5
hoit1011 <parksm071011@gmail.com>
2024-04-02 23:11:27 +0900

    19689_B4

---
+++ cpp/19689_B4.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 박성민 on 4/2/24.
+// 한줄평 : 그냥 쉬어가는 문제 ㅎㅎ; (캡스톤 하느라 시간이 업섰슴)
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    int n, w, h, l;
+//    cin >> n >> w >> h >> l;
+//    int ans = (w / l) * (h / l);
+//    n < ans ? cout << n : cout << ans;
+//}
