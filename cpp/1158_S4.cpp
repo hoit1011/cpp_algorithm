fdc2c486e672785277ad97707821f63d843bf0ec
hoit1011 <parksm071011@gmail.com>
2024-04-05 09:48:10 +0900

    1158_S4

--- cpp/1158_S4.cpp
+++ cpp/1158_S4.cpp
@@ -1,31 +1,31 @@
 //
 // Created by 박성민 on 4/5/24.
+// 한줄평 : 구현 방법은어떻게하는지 알았는데 구현 하는데 어렵다
+
+//#include <iostream>
+//#include <queue>
 //
-
-#include <iostream>
-#include <queue>
-
-using namespace std;
-
-queue<int> q;
-
-int main(){
-    int N,K;
-    cin >> N >> K;
-    for(int i = 1 ; i <= N; i++){
-        q.push(i);
-    }
-    cout << "<";
-    while(!q.empty()){
-        for(int i = 0; i < K - 1; i++){
-            int a = q.front();
-            q.pop();
-            q.push(a);
-        }
-        cout << q.front();
-        q.pop();
-        if(!q.empty())
-            cout << ',' << ' ';
-    }
-    cout << '>';
-}
+//using namespace std;
+//
+//queue<int> q;
+//
+//int main(){
+//    int N,K;
+//    cin >> N >> K;
+//    for(int i = 1 ; i <= N; i++){
+//        q.push(i);
+//    }
+//    cout << "<";
+//    while(!q.empty()){
+//        for(int i = 0; i < K - 1; i++){
+//            int a = q.front();
+//            q.pop();
+//            q.push(a);
+//        }
+//        cout << q.front();
+//        q.pop();
+//        if(!q.empty())
+//            cout << ',' << ' ';
+//    }
+//    cout << '>';
+//}
