235b17f4d9c9c5f4fe8ff0df0a8a3dabff85bfbc
hoit1011 <parksm071011@gmail.com>
2024-04-07 23:08:56 +0900

    21964_B4

---
+++ cpp/21964_B4.cpp
@@ -0,0 +1,12 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//    int n;
+//    cin >> n;
+//    string s;
+//    cin >> s;
+//    string s1 = s.substr(n-5,5);
+//    cout << s1;
+//}
