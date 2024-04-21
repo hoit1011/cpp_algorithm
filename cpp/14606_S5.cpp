89eb2efb34de4fd945651f658946c9148f16eacd
hoit1011 <parksm071011@gmail.com>
2024-04-22 08:18:35 +0900

    14606_S5

---
+++ cpp/14606_S5.cpp
@@ -0,0 +1,17 @@
+////
+//// Created by 박성민 on 4/19/24.
+////
+//
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//    int n;
+//    cin >> n;
+//    int cnt=0;
+//    for(int i = 1; i <= n - 1;i++){
+//        cnt += i;
+//    }
+//    cout << cnt;
+//}
