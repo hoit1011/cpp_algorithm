daf126c5e693e921c3ab58d55356d1ee710ddfd4
hoit1011 <parksm071011@gmail.com>
2024-05-07 13:56:24 +0900

    2935_B3

---
+++ cpp/2935_B3.cpp
@@ -0,0 +1,38 @@
+//
+// Created by 박성민 on 5/7/24.
+// 한줄평 : 사실 오랜만에 알고리즘 감을 잡기위해 쉬운문제를 골랐다고생각했는데 string으로 변수를 지정해놓고 '1'이렇게 넣어야하는걸 1로 넣어서 2시간동안 고민했던 문제였습니다 그거 빼면 쉬웠음.
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//    string a,b;
+//    char x;
+//    cin >> a;
+//    cin >> x;
+//    cin >> b;
+//    if(x == '+'){
+//        int num = 0;
+//        if(a.length() > b.length()) {
+//            num = a.length() - b.length();
+//            a[num] = '1';
+//            cout << a;
+//        }
+//        else if (a.length() == b.length()){
+//            a[0] = '2';
+//            cout << a;
+//        }
+//        else {
+//            num = b.length() - a.length();
+//            b[num] = '1';
+//            cout << b;
+//        }
+//    }else{
+//        cout << a;
+//        for(int i = 0; i < b.length() - 1; i++){
+//            cout << 0;
+//        }
+//    }
+//    return 0;
+//}
