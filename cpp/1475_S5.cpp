c7d5c469dce66f9044ba69d81f795ab06bd4db7e
hoit1011 <parksm071011@gmail.com>
2024-05-18 22:43:59 +0900

    1475_S5

---
+++ cpp/1475_S5.cpp
@@ -0,0 +1,19 @@
+//
+// Created by 박성민 on 5/18/24.
+//
+
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//
+//int main(){
+//    string str;
+//    cin >> str;
+//    int cnt[11] = {0,};
+//    for(int i = 0; i < str.length(); i++){
+//        cnt[str[i] - '0']++;
+//    }
+//    int sixnine = (cnt[6] + cnt[9] + 1) / 2;
+//    cnt[6] = cnt [9] = sixnine;
+//    cout << *max_element(cnt, cnt + 10);
+//}
