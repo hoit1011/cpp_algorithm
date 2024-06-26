cca068e603091a0ff58a168df7f830c42b476e5d
hoit1011 <parksm071011@gmail.com>
2024-06-26 10:38:23 +0900

    30030_B5

---
+++ cpp/30030_B5.cpp
@@ -0,0 +1,16 @@
+////
+//// Created by 박성민 on 6/26/24.
+////
+//
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//    int n;
+//    cin >> n;
+//    int num;
+//    num = n / 11;
+//    cout << n - num;
+//    return 0;
+//}
