b2abfbbd06ee9bff72343a7c0ef3b6616b777be6
hoit1011 <parksm071011@gmail.com>
2024-06-26 10:47:57 +0900

    23806_B3

---
+++ cpp/23806_B3.cpp
@@ -0,0 +1,27 @@
+//
+// Created by 박성민 on 6/26/24.
+// 좀 자신없었는데 잘푼듯 ㅎㅎ
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//    int n;
+//    cin >> n;
+//    for (int i = 1; i <= n*5; i++){
+//        for(int j = 1; j <= n*5; j++) {
+//            if (i <= n)
+//                cout << "@";
+//            else if(i > n*4)
+//                cout <<"@";
+//            else if(j <= n)
+//                cout << "@";
+//            else if(j > n*4)
+//                cout << "@";
+//            else
+//                cout << " ";
+//        }
+//        cout << '\n';
+//    }
+//}
