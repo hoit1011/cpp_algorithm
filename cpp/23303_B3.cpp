5941d719753b9e91a8c8c9a4dc2fbc6122963466
hoit1011 <parksm071011@gmail.com>
2024-06-26 10:32:15 +0900

    23303_B3

---
+++ cpp/23303_B3.cpp
@@ -0,0 +1,27 @@
+//
+// Created by 박성민 on 6/25/24.
+//
+//
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//    string s;
+//    getline(cin,s);
+//    bool D2 = false;
+//    for(int i = 0; i < s.length(); i++){
+//        if(s[i] == 'd' || s[i] == 'D'){
+//            if(s[i+1] == '2') {
+//                D2 = true;
+//                break;
+//            }
+//        }
+//    }
+//
+//    if(D2 == true)
+//        cout << "D2";
+//    else
+//        cout << "unrated";
+//    return 0;
+//}
