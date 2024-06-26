78c18be3a4ff01ac14779610a4045e4bb8b75560
hoit1011 <parksm071011@gmail.com>
2024-06-26 11:06:17 +0900

    10811_B2

---
+++ cpp/10811_B2.cpp
@@ -0,0 +1,26 @@
+//
+// Created by 박성민 on 6/26/24.
+// reverse를 어떻게 쓰는지 알았다
+
+//#include <iostream>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int main(){
+//    int N,M;
+//    cin >> N >> M;
+//    int arr[105] = {0,};
+//    for(int i = 0; i < N; i++){
+//        arr[i] = i+1;
+//    }
+//    for(int i = 0; i < M; i++){
+//        int a,b;
+//        cin >> a >> b;
+//        reverse(arr+a - 1, arr+b);
+//    }
+//    for(int i = 0; i < N; i++){
+//        cout << arr[i] << " ";
+//    }
+//    return 0;
+//}
